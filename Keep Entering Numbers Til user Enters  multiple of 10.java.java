b569d22ef2c50b548cfda43971e55de1c9b17dbf
vishpath <146863348+vishpathh@users.noreply.github.com>
2025-03-03 01:22:46 +0530

    Create Keep Entering Numbers Til user Enters  multiple of 10.java.java

---
+++ Keep Entering Numbers Til user Enters  multiple of 10.java.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class main{
+  public static void main (String args[]){
+      Scanner sc = new scanner(system.in);
+    do {
+    int n= sc.nextInt();
+      if (n%10==0){
+        break;
+        
+      } System.out.println();
+      
+    }while(true);
+    
+  }
+}
